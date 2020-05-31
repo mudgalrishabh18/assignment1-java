d5af7c86d537e9a5ad56f62f31b52ae5690252b0
Rishabh Mudgal <62346513+mudgalrishabh18@users.noreply.github.com>
2020-05-31 09:10:20 +0530

    Add files via upload

---
+++ TestSubsting.java
@@ -0,0 +1,13 @@
+//WAP of substring
+package assignment1;
+
+public class TestSubsting {
+
+		 public static void main(String args[]){  
+		   String s="SachinTendulkar";  
+		   System.out.println(s.substring(6));//Tendulkar  
+		   System.out.println(s.substring(0,6));//Sachin  
+		 }  
+		
+
+}
